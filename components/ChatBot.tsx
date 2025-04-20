a45a56c860d00862e9d7e1cf3adbd1471bd8020c
Poojitha Batchu <119473862+Poojitha-Batchu@users.noreply.github.com>
2025-04-20 16:23:23 +0530

    Update ChatBot.tsx

--- components/ChatBot.tsx
+++ components/ChatBot.tsx
@@ -103,7 +103,7 @@ const ChatBot = () => {
           onClick={() => setOpen(true)}
         >
           <Image 
-            src= "/public/images/chatbot.png"
+            src= "/images/chatbot.png"
             alt="Chat Bot" 
             width={60} 
             height={60}
