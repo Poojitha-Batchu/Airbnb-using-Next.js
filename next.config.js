21ee32be82a7e2373b3e60483f9de8b4aaa1ebe1
Poojitha Batchu <119473862+Poojitha-Batchu@users.noreply.github.com>
2025-04-20 17:46:58 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   output: 'export',
   // basePath: process.env.NODE_ENV === 'production' ? '/Airbnb-using-Next.js' : '',
   // assetPrefix: process.env.NODE_ENV === 'production' ? '/Airbnb-using-Next.js/' : '',
-  basePath: '/Airbnb-using-Next.js, 
+  basePath: '/Airbnb-using-Next.js', 
   images: {
     unoptimized: true,
   }
